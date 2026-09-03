a16e8097176571c8559a7fc233317135de197e7e
Csandjs <3423501494@qq.com>
2026-09-03 18:06:46 +0800

    添加第一个C#控制台程序HelloWorld

---
+++ HelloWord/Program.cs
@@ -0,0 +1,9 @@
+﻿// 这是注释，不会被执行
+string name = "小明";
+int age = 20;
+double height = 1.75;
+
+Console.WriteLine("大家好，我叫" + name);
+Console.WriteLine("我今年" + age + "岁");
+Console.WriteLine("我的身高是" + height + "米");
+Console.WriteLine("Hello, C#!");
