e3c72c4b300ba0e6bbd6ab7d8d852efe559a0b64
Csandjs <3423501494@qq.com>
2026-09-04 10:05:05 +0800

    2026.9.4 作业我的自我介绍

--- HelloWord/Program.cs
+++ HelloWord/Program.cs
@@ -1,9 +1,2 @@
-﻿// 这是注释，不会被执行
-string name = "小明";
-int age = 20;
-double height = 1.75;
-
-Console.WriteLine("大家好，我叫" + name);
-Console.WriteLine("我今年" + age + "岁");
-Console.WriteLine("我的身高是" + height + "米");
-Console.WriteLine("Hello, C#!");
+﻿Console.WriteLine("I Love DJS");
+Console.ReadKey();
---
+++ selfintro/Program.cs
@@ -0,0 +1,9 @@
+﻿string name = "小陈";
+int age = 21;
+double height = 170.0;
+string hobby ="听歌";
+Console.WriteLine("大家好，我的名字叫做"+name);
+Console.WriteLine("我今年"+age+"岁");
+Console.WriteLine("身高"+height+"米");
+Console.WriteLine("我的爱好是"+hobby);
+Console.ReadKey();
